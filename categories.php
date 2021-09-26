5d3bf42b70e6bcb8a701c79e1c4b42aa5547605b
duychip0909 <duychip9901@gmail.com>
2021-09-26 23:04:06 +0700

    final01

---
+++ categories.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/categories.css">
+  <title>WOW Foods!</title>
+</head>
+
+<body>
+  <!-- Heading -->
+  <div class="container-fluid header">
+    <div class="column">
+      <div class="row">
+        <div class="col-md-6 logo">
+          <img src="img/logo.png" alt="Responsive IMG" class="logo img-fluid">
+        </div>
+        <div class="col-md-6">
+          <ul class="nav justify-content-center submenu">
+            <li class="nav-item">
+              <a class="nav-link active text-danger" aria-current="page" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-danger" href="#">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-danger" href="food.php">Foods</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-danger">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End heading -->
+  <!-- Start Body -->
+  <div class="container-fluid">
+    <div class="column">
+      <div class="row cuatoi">
+        <div class="col-md-12">
+          <p class="EF">Explore Foods</p>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="row">
+        <div class="col-md-12 cuatoi">
+          <img src="img/pizza.jpg" alt="" class="rounded-float-start pdf">
+          <img src="img/burger.jpg" alt="" class="pdf">
+          <img src="img/momo.jpg" alt="" class="rounded-float-end pdf">
+        </div>
+
+        <div class="col-md-12 cuatoi">
+          <img src="img/pizza.jpg" alt="" class="rounded-float-start pdf">
+          <img src="img/burger.jpg" alt="" class="pdf">
+          <img src="img/momo.jpg" alt="" class="rounded-float-end pdf">
+        </div>
+
+        <div class="col-md-12 cuatoi">
+          <img src="img/pizza.jpg" alt="" class="rounded-float-start pdf">
+          <img src="img/burger.jpg" alt="" class="pdf">
+          <img src="img/momo.jpg" alt="" class="rounded-float-end pdf">
+        </div>
+
+        <div class="col-md-12 cuatoi">
+          <img src="img/pizza.jpg" alt="" class="rounded-float-start pdf">
+          <img src="img/burger.jpg" alt="" class="pdf">
+          <img src="img/momo.jpg" alt="" class="rounded-float-end pdf">
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <!-- End body -->
+  <!-- Open Footer -->
+  <div class="container-fluid">
+    <div class="column">
+      <div class="row">
+      <div class="icon-group icon">
+            <a href="#">
+                <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" class="icon1">
+            </a>
+            <a href="#">
+                <img src="https://img.icons8.com/cute-clipart/64/000000/instagram-new.png" class="icon1">
+            </a>
+            <a href="#">
+                <img src="https://img.icons8.com/color/48/000000/twitter--v1.png" class="icon1">
+            </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+        <div class="footer-title text-center">
+            <p>All rights reserved. Designed by 
+                <a href="#" style="color:salmon" class="footertit">Nguyen Duc Duy</a>
+            </p>
+        </div>
+    </div>
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ food.php
@@ -0,0 +1,180 @@
+<!-- <!doctype html>
+<html lang="en">
+
+<head>
+  
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+ Bootstrap CSS -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<link rel="stylesheet" href="css/home.css">
+<title>WOW Foods!</title>
+</head>
+
+<body>
+    <div class="container-fluid header">
+        <div class="column">
+            <div class="row">
+                <div class="col-md-6 logo">
+                    <img src="img/logo.png" alt="Responsive IMG" class="logo img-fluid">
+                </div>
+                <div class="col-md-6">
+                    <ul class="nav justify-content-center submenu">
+                        <li class="nav-item">
+                            <a class="nav-link active text-danger" aria-current="page" href="/Boostrap/home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-danger" href="categories.php">Categories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-danger" href="food.php">Foods</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-danger">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="column">
+            <div class="row">
+                <div class="col-md-12 bg-image hbg1">
+                    <input type="search" class="form" placeholder="Search for Food...">
+                    <button class="button">Search</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <div class="container-fluid" style="background-color: lightgray;">
+        <div class="column">
+            <div class="row">
+                <div class="col-md-12 cuatoi">
+                    <p class="EF">Food Menu</p>
+                </div>
+
+
+                <div class="column" style="padding-left:10%; margin-top:15px">
+                    <div class="row row-cols-1 row-cols-md-2 g-4">
+                        <div class="col">
+                            <div class="card mb-3 pdf" style="max-width: 540px; padding:10px; margin-left:30px">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="img/menu-pizza.jpg" class="img-fluid rounded-float pdf" alt="...">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Food Title</h5>
+                                            <p class="card-text">$2.3</p>
+                                            <p class="card-text"><small class="text-muted">Made with Italian Sauce, Chicken, and organice vegetables</small></p>
+                                            <a href="#" class="btn btn-danger">Order Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card mb-3 pdf" style="max-width: 540px; padding:10px">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="img/menu-burger.jpg" class="img-fluid rounded-float pdf" alt="...">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Smoky Burger</h5>
+                                            <p class="card-text">$2.3</p>
+                                            <p class="card-text"><small class="text-muted">Made with Italian Sauce, Chicken, and organice vegetables</small></p>
+                                            <a href="#" class="btn btn-danger">Order Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card mb-3 pdf" style="max-width: 540px; padding:10px; margin-left:30px">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="img/menu-burger.jpg" class="img-fluid rounded-float pdf" alt="...">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nice Burger</h5>
+                                            <p class="card-text">$2.3</p>
+                                            <p class="card-text"><small class="text-muted">Made with Italian Sauce, Chicken, and organice vegetables</small></p>
+                                            <a href="#" class="btn btn-danger">Order Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card mb-3 pdf" style="max-width: 540px; padding:10px; margin-right:30px">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="img/menu-pizza.jpg" class="img-fluid rounded-float pdf" alt="...">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Food Title </h5>
+                                            <p class="card-text">$2.3</p>
+                                            <p class="card-text"><small class="text-muted">Made with Italian Sauce, Chicken, and organice vegetables</small></p>
+                                            <a href="#" class="btn btn-danger">Order Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-fluid">
+                    <a href="#" style="text-decoration: none;">
+                        <p style="text-align: center; color:indianred">See All Foods</p>
+                    </a>
+                </div>
+
+            
+
+
+
+
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+     
+</body>
+<footer style="padding: 0px;">
+    <div class="container-fluid" style="background-color: white; padding:0px">
+        <div class="column">
+            <div class="row">
+                <div class="icon-group icon">
+                    <a href="#">
+                        <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" class="icon1">
+                    </a>
+                    <a href="#">
+                        <img src="https://img.icons8.com/cute-clipart/64/000000/instagram-new.png" class="icon1">
+                    </a>
+                    <a href="#">
+                        <img src="https://img.icons8.com/color/48/000000/twitter--v1.png" class="icon1">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid" style="background-color: white; padding:0px">
+        <div class="footer-title text-center">
+            <p>All rights reserved. Designed by
+                <a href="#" style="color:salmon" class="footertit">Nguyen Duc Duy</a>
+            </p>
+        </div>
+    </div>
+</footer>
+
+</html>
